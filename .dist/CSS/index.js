299c1c0f5e5a1b359575b6a8c0ed7dbb8fabaed9
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2020-12-27 16:51:17 -0500

    added onmouseover effect on lines 44-46

--- .dist/CSS/index.js
+++ .dist/CSS/index.js
@@ -1,6 +1,10 @@
-$(document).ready(function(){
-    $(".box").hover(
-        function() {$(this).attr("src","images/milkyway.png");},
-        function() {$(this).attr("src","images/about.png");
-    });
-});
+var image = document.getElementById("image");
+
+//Now, we need to add an Event Listener to listen when the image gets mouse over.
+
+image.addEventListener('mouseover', function(){
+  image.src = "path/to/newimage"
+})
+image.addEventListener('mouseout', function(){
+    image.src = "path/to/otherimage"
+  })
